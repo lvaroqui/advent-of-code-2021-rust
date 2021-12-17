86e9f775947a1fb69b55598e819578d2d7acc85e
Luc Varoqui <luc@varoqui.org>
2021-12-17 16:52:47 +0100

    Solve day 2

---
+++ src/day2.rs
@@ -0,0 +1,81 @@
+use std::str::FromStr;
+
+use crate::{parse_line, Lines};
+
+enum Command {
+    Forward,
+    Down,
+    Up,
+}
+
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "forward" => Ok(Command::Forward),
+            "down" => Ok(Command::Down),
+            "up" => Ok(Command::Up),
+            _ => Err(()),
+        }
+    }
+}
+
+struct Line {
+    cmd: Command,
+    value: u32,
+}
+
+impl FromStr for Line {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let words = s.split(' ').collect::<Vec<_>>();
+        Ok(Line {
+            cmd: Command::from_str(words[0])?,
+            value: words[1].parse::<u32>().unwrap(),
+        })
+    }
+}
+
+pub struct Solver1 {}
+
+impl crate::Solver for Solver1 {
+    fn solve(self: &mut Self, lines: Lines) -> String {
+        let mut x = 0;
+        let mut d = 0;
+
+        for line in lines.map(parse_line::<Line>) {
+            match line.cmd {
+                Command::Down => d += line.value,
+                Command::Up => d -= line.value,
+                Command::Forward => x += line.value,
+            }
+        }
+
+        (x * d).to_string()
+    }
+}
+
+pub struct Solver2 {}
+
+impl crate::Solver for Solver2 {
+    fn solve(self: &mut Self, lines: Lines) -> String {
+        let mut x = 0;
+        let mut d = 0;
+        let mut aim = 0;
+
+        for line in lines.map(parse_line::<Line>) {
+            match line.cmd {
+                Command::Down => aim += line.value,
+                Command::Up => aim -= line.value,
+                Command::Forward => {
+                    x += line.value;
+                    d += aim * line.value;
+                }
+            }
+        }
+
+        (x * d).to_string()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use std::fs::File;
 use std::io::{self, BufReader};
+use std::str::FromStr;
 
 mod day1;
+mod day2;
 
 pub trait Solver {
     fn solve(self: &mut Self, _lines: Lines) -> String {
@@ -30,10 +32,20 @@ impl Iterator for Lines {
     }
 }
 
+pub fn parse_line<T>(line: String) -> T
+where
+    T: FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    T::from_str(&line).unwrap()
+}
+
 pub fn get_solver(day: u32, part: u32) -> Box<dyn Solver> {
     match (day, part) {
         (1, 1) => Box::new(day1::Solver1 {}),
         (1, 2) => Box::new(day1::Solver2 {}),
+        (2, 1) => Box::new(day2::Solver1 {}),
+        (2, 2) => Box::new(day2::Solver2 {}),
         _ => panic!("Day {} part {} is not implemented yet", day, part),
     }
 }
